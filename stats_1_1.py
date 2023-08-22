e639d0e0e3fa3175ba11768cc334c3a12722b78d
Maryam Salarhaji <93576036+MaryamSalarhaji@users.noreply.github.com>
2023-08-22 16:16:11 +0330

    Add files via upload

---
+++ stats_1_1.py
@@ -0,0 +1,33 @@
+import random
+#This code eliminate input that are not number and are #strings
+while True:
+    try:
+        num=int(input("Please enter a number:"))
+        break
+    except:
+        print("Oops!  That was no valid number")
+    continue    
+count=0
+tot=0
+smallest=None
+largest=None
+while True: 
+             
+    x=random.random()
+    count=count+1
+    tot=tot+x
+    avg=tot/count
+    print(x)  
+    if count==num:
+        break
+    
+    if smallest is None:
+        smallest=x
+    elif x<smallest:
+        smallest=x
+    if largest is None:
+        largest=x
+    elif x>largest:
+        largest=x
+             
+print("Average is:", avg, "Smallest is:", smallest, "Largest is:", largest) 
